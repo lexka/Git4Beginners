1c6b68e323f3a9bc1067e3962fb90910e46dc184
Alex Kamdem Mambou <mambou14@yahoo.fr>
2020-10-14 21:40:54 +0200

    remove this java project

---
+++ HelloTest/src/BonjourLeMonde.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author lexka
+ *
+ */
+public class BonjourLeMonde {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Test: Bonjourle monde!");
+	}
+
+}
