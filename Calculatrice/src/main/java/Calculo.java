0977418fa5e8dbde0179856ebfa3288b19041b87
Alex Kamdem Mambou <mambou14@yahoo.fr>
2020-10-18 01:08:40 +0200

    calculatrice

---
+++ Calculatrice/src/main/java/Calculo.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+
+/**
+ * @author lexka
+ *
+ */
+public class Calculo {
+	
+	static double addition(double numA, double numB)
+	{
+		return (numA + numB);
+	}
+	
+	static double soutraction(double numA, double numB)
+	{
+		return (numA - numB);
+	}
+	
+	static double multiplication(double numA, double numB)
+	{
+		return (numA * numB);
+	}
+	
+	static double Square(double numA)
+	{
+		return (numA * numA);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double d_numA, d_numB;
+		d_numA = 5;
+		d_numB = 3;
+				
+		System.out.println("SOME ARITHMETIC OPERATIONS:\n");
+		System.out.println("1.Addition:");
+		System.out.println(" "+ d_numA +" + "+d_numB +" = " + addition(d_numA, d_numB) + "\n");
+		System.out.println("2.Soustraction:");
+		System.out.println(" "+ d_numA +" - "+d_numB +" = " + soutraction(d_numA, d_numB)+ "\n");
+		System.out.println("3.Multiplication:");
+		System.out.println(" "+ d_numA +" x "+d_numB +" = " + multiplication(d_numA, d_numB)+ "\n");
+		System.out.println("4.Square:");
+		System.out.println(" Square("+ d_numA +") = " + Square(d_numA)+ "\n");
+	}
+
+}
